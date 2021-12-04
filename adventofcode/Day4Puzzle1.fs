c7d671abd943f843a888e43c51518b446fdc8082
Martin Camitz <martin.camitz@cocoin.com>
2021-12-04 21:33:21 +0100

    hasBingo appears to work

--- adventofcode/Day4Puzzle1.fs
+++ adventofcode/Day4Puzzle1.fs
@@ -4,7 +4,7 @@ open System
 
 let lines = IO.File.ReadLines (__SOURCE_DIRECTORY__ + @"\day4boards.txt")
 
-let draws = (Seq.head lines).Split ','
+let draws = (Seq.head lines).Split ',' |> Seq.map Convert.ToInt32
 
 let boards = Seq.skip 2 lines 
             |> Seq.chunkBySize 6
@@ -12,10 +12,19 @@ let boards = Seq.skip 2 lines
             |> Seq.map (fun board -> board 
                                     |> Seq.map (fun row -> row.Split(' ', StringSplitOptions.RemoveEmptyEntries))
                                     |> Seq.map (fun row -> row
-                                                        |> Seq.map (fun number -> Convert.ToInt32(number.Trim()))
+                                                        |> Seq.map Convert.ToInt32
                                                         |> Seq.toArray)
                                     |> Seq.toArray)
             |> Seq.map (fun board -> Array2D.init 5 5 (fun i j -> board.[i].[j]))
 
 let b = boards |> Seq.head
-let puzzle1 = b.[1,*]
+
+let hasBingo (board : int [,]) draws =
+     ([0..4] |> Seq.exists (fun r -> (Set.intersect (set draws) (set board.[r,*])).Count = 5),
+      [0..4] |> Seq.exists (fun c -> (Set.intersect (set draws) (set board.[*,c])).Count = 5))  
+        <> (false,false)
+        
+
+let puzzle1 = 
+    printfn "%A" (Seq.toList (Seq.take 14 draws))
+    hasBingo b (Seq.take 14 draws)
