753ada71a327c4971e6b1ce86501750c26fb5b6c
Martin Camitz <martin.camitz@cocoin.com>
2021-12-08 16:52:10 +0100

    Day 5, puzzle 1 solved

---
+++ adventofcode/Day5.fs
@@ -0,0 +1,52 @@
+﻿module Day5
+
+open System 
+
+type Direction =
+    | Horz
+    | Vert
+
+
+//Prepare the datastructures
+let fileLines = IO.File.ReadLines (__SOURCE_DIRECTORY__ + @"\day5coords.txt")
+
+let lines = fileLines 
+            |> Seq.map (fun x -> x.Split (" -> ,".ToCharArray(),StringSplitOptions.RemoveEmptyEntries))
+            |> Seq.map (fun x ->  Array.map (fun (y:string) -> Convert.ToInt32 y) x)
+            |> Seq.map (fun x -> match x with
+                            | [|a;b;c;d;|] -> ((a,b),(c,d)))
+
+let x1 line = fst (fst line)
+let x2 line  = fst (snd line)
+let y1 line  = snd (fst line)
+let y2 line  = snd (snd line)
+
+let direction line =
+    match line with 
+        | _ when x1 line = x2 line -> Some Vert
+        | _ when y1 line = y2 line -> Some Horz
+        | _ -> None
+    
+let range line x1 x2 =
+    match (x1 line, x2 line) with
+    | (a,b) when a > b -> set[b..a]
+    | (a,b) -> set[a..b]
+
+let coveredPoints line =
+    match direction line with 
+        | Some d when d = Vert -> range line y1 y2 |> Set.map (fun y -> (x1 line, y))
+        | Some d when d = Horz -> range line x1 x2 |> Set.map (fun x -> (x, y1 line))
+        | _ -> Set.empty
+
+
+let doubleCoveredPoints = 
+    lines 
+    |> Seq.map (fun line -> coveredPoints line)
+    |> Seq.concat
+    |> Seq.groupBy id
+    |> Seq.choose (fun (k,v) -> match Seq.length v with        
+                                         | x when x >= 2 -> Some k
+                                         | _            -> None)
+
+let puzzle1 = Seq.length doubleCoveredPoints 
+    
