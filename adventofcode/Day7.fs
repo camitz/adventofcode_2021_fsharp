54c5f1aaba1fa49730f4c5066634ba6dd584708c
Martin Camitz <martin.camitz@cocoin.com>
2021-12-09 23:56:16 +0100

    Day 7 puzzle2 solved

--- adventofcode/Day7.fs
+++ adventofcode/Day7.fs
@@ -12,9 +12,21 @@ let positions = (Seq.exactlyOne lines).Split ','
 let costToMove p positions =
     positions |> Seq.sumBy (fun x -> abs (x-p))
 
+let costInc p0 p1 =
+    let a = abs (p0-p1) 
+    a * (a+1) / 2
+
+let costToMoveInc p1 positions =
+    positions 
+        |> Seq.sumBy (fun p0 -> costInc p0 p1)
 
 let puzzle1 = 
     positions
     |> Seq.map (fun p -> costToMove p positions)
+    |> Seq.min
+
+let puzzle2 = 
+    [1..Seq.last positions]
+    |> Seq.map (fun p1 -> costToMoveInc p1 positions)
     |> Seq.min
 
