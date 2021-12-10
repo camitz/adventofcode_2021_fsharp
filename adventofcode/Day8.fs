9fe78909589a3d14cabbc02c83493682e609cadd
Martin Camitz <martin.camitz@cocoin.com>
2021-12-10 13:39:33 +0100

    Refactor

--- adventofcode/Day8.fs
+++ adventofcode/Day8.fs
@@ -5,13 +5,16 @@ open System
 //Prepare the datastructures
 let lines = IO.File.ReadLines (__SOURCE_DIRECTORY__ + @"\day8readings.txt")
 
-let readings = lines |>
-                    Seq.map (fun l -> l.Split(" |".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+let input = fst
+let output = snd
+
+let readings = lines 
+                    |> Seq.map (fun l -> l.Split(" |".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+                    |> Seq.map (fun rs -> (rs.[0..9], rs.[10..]))
 
 let puzzle1 = 
     readings 
-    |> Seq.map (fun rs -> rs.[10..])
-    |> Seq.map (fun os -> os|> 
+    |> Seq.map (fun rs -> output rs |> 
                             Seq.sumBy (fun o -> if o.Length <=4 or o.Length = 7 then 1 else 0))
     |> Seq.sum
 
