f702a5b9c80970902aa5a29aee20c3e9f23cf47a
Martin Camitz <martin.camitz@cocoin.com>
2021-12-10 14:55:42 +0100

    Working on deduce

--- adventofcode/Day8.fs
+++ adventofcode/Day8.fs
@@ -7,6 +7,7 @@ let lines = IO.File.ReadLines (__SOURCE_DIRECTORY__ + @"\day8readings.txt")
 
 let input = fst
 let output = snd
+let single = Seq.exactlyOne
 
 let readings = lines 
                     |> Seq.map (fun l -> l.Split(" |".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
@@ -18,13 +19,38 @@ let puzzle1 =
                             Seq.sumBy (fun o -> if o.Length <=4 or o.Length = 7 then 1 else 0))
     |> Seq.sum
 
-let segmentCount inputs =
-    inputs
-    |> Seq.collect id
-    |> Seq.groupBy id
-    |> Seq.map (fun (s,n) -> (s, Seq.length n))
+let digitsOfLength m =
+    Seq.filter (fun (i:string) -> i.Length = m)
+
+let segmentCount =
+    Seq.collect id >>
+    Seq.groupBy id >>
+    Seq.map (fun (s,n) -> (s, Seq.length n))
+
+let deduceConnections inputs =
+    let segemenstWithCount m =
+        segmentCount inputs
+        |> Seq.choose (fun(s,n)-> if n = m then Some s else None)
+
+    [
+     yield ("e", segemenstWithCount 4 |> single)
+     yield ("b", segemenstWithCount 6 |> single)
+     yield ("f", segemenstWithCount 9 |> single)
+     //yield ("a", Set.intersect 
+     //                   (set(segemenstWithCount 8)) 
+     //                   (set(digitsOfLength 2 inputs))
+     //               |> single
+     //      )
+    ]
+
+let puzzle = 
+    input (readings |> Seq.head)
+    |> segmentCount
+    |> Seq.toList
 
 let puzzle2 = 
-    (input >> segmentCount) (readings |> Seq.head)
+    input (readings |> Seq.head)
+    |> deduceConnections 
+    //|> digitsOfLength 2
     |> Seq.toList
 
