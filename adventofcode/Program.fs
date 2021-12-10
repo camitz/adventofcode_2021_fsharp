289153d019b5568b14a5bc96b18416590c25ade4
Martin Camitz <martin.camitz@cocoin.com>
2021-12-10 13:36:35 +0100

    Day 8 puzzle 1 solved

--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -2,19 +2,20 @@
 
 [<EntryPoint>]
 let main argv =
-    printfn "Day 7, puzzle 2: %d" Day7.puzzle2
-    printfn "Day 7, puzzle 1: %d" Day7.puzzle1
-    printfn "Day 6, puzzle 2: %d" Day6Puzzle2.puzzle2
-    printfn "Day 6, puzzle 1: %d" Day6Puzzle1.puzzle1
-    printfn "Day 5, puzzle 2: %d" Day5.puzzle2
-    printfn "Day 5, puzzle 1: %d" Day5.puzzle1
-    printfn "Day 4, puzzle 2: %d" Day4.puzzle2
-    printfn "Day 4, puzzle 1: %d" Day4.puzzle1
-    printfn "Day 3, puzzle 2: %d" Day3Puzzle2.puzzle2
-    printfn "Day 3, puzzle 1: %d" Day3Puzzle1.puzzle1
-    printfn "Day 2, puzzle 2: %d" Day2Puzzle2.puzzle2
-    printfn "Day 2, puzzle 1: %d" Day2Puzzle1.puzzle1
-    printfn "Day 1, puzzle 1: %d" Day1.puzzle1
-    printfn "Day 1, puzzle 2: %d" Day1.puzzle2
+    printfn "Day 8, puzzle 1: %A" Day8.puzzle1
+    //printfn "Day 7, puzzle 2: %d" Day7.puzzle2
+    //printfn "Day 7, puzzle 1: %d" Day7.puzzle1
+    //printfn "Day 6, puzzle 2: %d" Day6Puzzle2.puzzle2
+    //printfn "Day 6, puzzle 1: %d" Day6Puzzle1.puzzle1
+    //printfn "Day 5, puzzle 2: %d" Day5.puzzle2
+    //printfn "Day 5, puzzle 1: %d" Day5.puzzle1
+    //printfn "Day 4, puzzle 2: %d" Day4.puzzle2
+    //printfn "Day 4, puzzle 1: %d" Day4.puzzle1
+    //printfn "Day 3, puzzle 2: %d" Day3Puzzle2.puzzle2
+    //printfn "Day 3, puzzle 1: %d" Day3Puzzle1.puzzle1
+    //printfn "Day 2, puzzle 2: %d" Day2Puzzle2.puzzle2
+    //printfn "Day 2, puzzle 1: %d" Day2Puzzle1.puzzle1
+    //printfn "Day 1, puzzle 1: %d" Day1.puzzle1
+    //printfn "Day 1, puzzle 2: %d" Day1.puzzle2
     0 // return an integer exit code
 
